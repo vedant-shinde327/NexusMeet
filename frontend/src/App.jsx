41f8c656b2353614fa784ea15238ad81ac213147
vedant-shinde327 <vedantbaban.shinde@gmail.com>
2026-08-06 14:03:24 +0530

    building login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import './App.css';
 import LandingPage from "./pages/LandingPage";
+import Authentication from "./pages/Authentication";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/auth" element={<Authentication />} />
       </Routes>
     </>
   );
--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,8 +1,43 @@
+import Logo1 from "../../public/favicon.svg";
+import heroImg from "../assets/mobile.png";
+import { Link } from "react-router-dom"; 
+
 export default function LandingPage() {
   return (
     <div>
-      <h1>landing page</h1>
-    </div>
-  )
-}
+      <div className="landingPageContainer">
+        <nav>
+          <div className="navHeader">
+            <img src={Logo1} alt="NexusLogo" className="NexusLogo" />
+            <h2 className="NexusHeading">NexusMeet</h2>
+          </div>
 
+          <div className="navList">
+            <p>Join as Guest</p>
+            <p>Register</p>
+            <div className="loginBtn" role="button">
+              Login
+            </div>
+          </div>
+        </nav>
+
+        <div className="landingMainContainer">
+          <div>
+            <h1>
+              <span style={{ color: "#FF9839" }}>Connect</span> with your Loved
+              ones
+            </h1>
+            <p>Meet Anyone. Anywhere. Instantly.</p>
+            <div role="button">
+              <Link to={"/auth"}>Get Started</Link>
+            </div>
+          </div>
+
+          <div>
+            <img src={heroImg} alt="heroImg" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
